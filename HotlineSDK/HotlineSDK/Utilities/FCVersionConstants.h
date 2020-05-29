a65f3efe0e496a3af88497a0d753a69f19e5e0ae
Hemanth Kumar <hemanth.kumar@freshworks.com>
2020-05-29 15:29:40 +0530

    Updated Release notes and version to 3.6.1

--- HotlineSDK/HotlineSDK/Utilities/FCVersionConstants.h
+++ HotlineSDK/HotlineSDK/Utilities/FCVersionConstants.h
@@ -9,7 +9,7 @@
 #ifndef FCVersionConstants_h
 #define FCVersionConstants_h
 
-#define FRESHCHAT_SDK_VERSION @"3.6.2"
-#define FRESHCHAT_SDK_BUILD_NUMBER @"362"
+#define FRESHCHAT_SDK_VERSION @"3.6.1"
+#define FRESHCHAT_SDK_BUILD_NUMBER @"361"
 
 #endif /* FCVersionConstants_h */
