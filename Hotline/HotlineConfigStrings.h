22b99e5591a2f53aab9df410a23eaa8167db366b
Harish kumar <harish.kumar@freshworks.com>
2020-06-12 14:10:24 +0530

    Update account appid and keys

--- Hotline/HotlineConfigStrings.h
+++ Hotline/HotlineConfigStrings.h
@@ -1,30 +1,20 @@
 
 #define ENABLE_RTL_RUNTIME 1
 #define PUSH_ENABLED 1
-#define HOTLINE_DOMAIN @"threedoto.freshpori.com"
+#define HOTLINE_DOMAIN @"msdk.freshchat.com"
 
 //sanjith.kanagavel+web@freshworks.com/testtest
 //#define HOTLINE_APP_ID @"7558e847-515b-4688-9d64-638496e0f7c3"
 //#define HOTLINE_APP_KEY @"ef99705a-4a49-4274-afef-9622bd404e0e"
 
-//vidya.anand+pod@freshworks.com/test@123
-//#define HOTLINE_APP_ID @"251869c4-d037-4427-8859-bfadcd8c6d3c"
-//#define HOTLINE_APP_KEY @"5577c079-31bd-415b-abc1-a21eabd5cbb0"
+//Peacock app
+//#define HOTLINE_APP_ID @"c69641e9-8a85-4da1-858e-77169b0c76a7"
+//#define HOTLINE_APP_KEY @"23d7240c-0176-4de1-acc4-d63c534b59be"
 
 //harish.kumar+freshchat1@freshworks.com/testtest123
-#define HOTLINE_APP_ID @"b123ea17-dd46-4e6e-be8e-415a117b5106"
-#define HOTLINE_APP_KEY @"b80a4adc-1943-4e02-80ef-fa9c733d2898"
+#define HOTLINE_APP_ID @"63d96e60-642f-48cb-b40a-dfbf16306fe8"
+#define HOTLINE_APP_KEY @"60b7af4d-5729-45e2-b94f-9ea1af8bad20"
 
-//JWT Prod Account
+//Freshchat JWT Test Account
 //#define HOTLINE_APP_ID @"e69e1372-9e49-4cb1-b6b6-daa59e6b3f80"
 //#define HOTLINE_APP_KEY @"8cb728bb-a96e-480b-a376-7077602137cb"
-
-//JWT Enabled
-//#define HOTLINE_DOMAIN @"mr-brown.freshpori.com"
-//#define HOTLINE_APP_ID @"c32adad9-31b7-417e-b7e7-9c747ec010b2"
-//#define HOTLINE_APP_KEY @"7e812471-0115-4f1b-aeb8-54cd9b406e14"
-
-//Non-JWT Enabled
-//#define HOTLINE_DOMAIN @"mr-brown.freshpori.com"
-//#define HOTLINE_APP_ID @"fa9d4fb4-d2b4-4bd0-8e9a-15f0bc2a9ca6"
-//#define HOTLINE_APP_KEY @"b182ab15-1ed9-4c96-ac8c-bdf0c486c83d"
